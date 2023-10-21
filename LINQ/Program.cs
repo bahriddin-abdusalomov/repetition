5c4071f181c53488cc29530aa504d3a76da3156c
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-21 22:40:35 +0500

    learing LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using LINQ.MethodSyntax;
+using LINQ.MixedSyntax;
+using LINQ.Models;
+using LINQ.QuerySyntax;
+using System.Collections.Generic;
+
+
+// Method syntax
+//MethodSyntaxExample.Run();
+
+//Query syntax
+//QuerySyntaxExample.Run
+
+//Mixed syntax
+//MixedSyntaxExample.Run();
+
+
+var employee = from emp in Employee.GetAllAsync() select new Employee()
+{
+    FirstName = emp.FirstName,
+    Position = emp.Position,
+    Salary = emp.Salary * 12,
+};
+
+var result = Employee.GetAllAsync().SelectMany(employee => employee.ProgrammingLanguages,
+    (employee, language) => new { employee.FirstName, Language = language });
+
+foreach (var item in result)
+{
+    Console.WriteLine(item.FirstName + " " + item.Language);
+}
+
