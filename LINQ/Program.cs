0567d073cad44ba65f5ff8d282d57b1649669541
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-22 21:37:10 +0500

    orderBy, thenBy, any, all

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using LINQ.MethodSyntax;
+﻿using LINQ;
+using LINQ.MethodSyntax;
 using LINQ.MixedSyntax;
 using LINQ.Models;
 using LINQ.QuerySyntax;
@@ -14,19 +15,54 @@ using System.Collections.Generic;
 //Mixed syntax
 //MixedSyntaxExample.Run();
 
+// Select and SelectMany
+//var employee = from emp in Employee.GetAllAsync() select new Employee()
+//{
+//    FirstName = emp.FirstName,
+//    Position = emp.Position,
+//    Salary = emp.Salary * 12,
+//};
 
-var employee = from emp in Employee.GetAllAsync() select new Employee()
-{
-    FirstName = emp.FirstName,
-    Position = emp.Position,
-    Salary = emp.Salary * 12,
-};
+//var result = Employee.GetAllAsync().SelectMany(employee => employee.ProgrammingLanguages,
+//    (employee, language) => new { employee.FirstName, Language = language });
 
-var result = Employee.GetAllAsync().SelectMany(employee => employee.ProgrammingLanguages,
-    (employee, language) => new { employee.FirstName, Language = language });
+//var result2 = Employee.GetAllAsync().Select(employee => new 
+//{
+//    Name = employee.FirstName,
+//    ProgrammingLanguages  = string.Join(" ", employee.ProgrammingLanguages),
+//    Tools = string.Join(" ", employee.Tools)
+//});
 
-foreach (var item in result)
-{
-    Console.WriteLine(item.FirstName + " " + item.Language);
-}
+//foreach (var item in result2)
+//{
+//    Console.WriteLine(item.Name);
+//    Console.WriteLine(item.ProgrammingLanguages);
+//    Console.WriteLine(item.Tools);
+//}
 
+
+// OrderBy OrderByDescending and ThenBy ThenByDescending
+
+
+//var orderByMethod = Employee.GetAllAsync().OrderBy(emp => emp.Id).ThenBy(emp => emp.LastName).ToList();
+
+//orderByMethod.ForEach(emp => Console.WriteLine($"{emp.Id}: {emp.LastName}"));
+
+//var studentOrder = Student.GetAllStudents().OrderBy(studenet =>  studenet.FirstName)
+//    .ThenByDescending(student => student.Course)
+//    .ThenBy(student => student.Contract)
+//    .ThenByDescending(student => student.Age).ToList();
+
+//studentOrder.ForEach(student => Console.WriteLine($"{student.FirstName} {student.Course} {student.Contract} {student.Age}"));
+
+//var studentOrderQuery = (from student in Student.GetAllStudents() 
+//                         orderby student.FirstName,
+//                         student.Course descending, 
+//                         student.Contract, 
+//                         student.Age descending 
+//                         select student);
+
+var avgStudentContract = Student.GetAllStudents().Average(student => student.Contract);
+Console.WriteLine(avgStudentContract);
+
+var avgQueryContract = from studnet in Student.GetAllStudents() average
